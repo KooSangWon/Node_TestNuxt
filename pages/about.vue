33a648d67ecd35ee2b542a3dbbf6566d1becd2ce
KooSangWon <gsw0931@gmail.com>
2018-05-08 13:12:41 +0900

    [2018-05-08] TestNuxt#1

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="section">
+      <div class="block">
+        <a href="#" class="button is-primary">Test</a><br>
+        <p>클라우드 서비스용 포트폴리오</p>
+        <img src="~static/logo.gif" alt=""><br>
+        <img src="~assets/lemon.jpg" alt="">
+      </div>
+      <br>
+      <nuxt-link to="/about">About</nuxt-link>
+    </section>
+  </template>
+  
